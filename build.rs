ea43d018805941a75cda5f3cd04e1b481c3d7305
Rémi Garde <remi.garde@free.fr>
2019-01-23 11:53:16 +0100

    build: assert that the commands worked
    In particular check for GCC compilation without errors

--- build.rs
+++ build.rs
@@ -7,12 +7,15 @@ fn main() {
 
     // note that there are a number of downsides to this approach, the comments
     // below detail how to improve the portability of these commands.
-    Command::new("gcc").args(&["src/cimd.c", "-c", "-march=native", "-O3", "-o"])
+    let mut status = Command::new("gcc").args(&["src/cimd.c", "-c", "-march=native", "-O3", "-o"])
                        .arg(&format!("{}/cimd.o", out_dir))
                        .status().unwrap();
-    Command::new("ar").args(&["crus", "libcimd.a", "cimd.o"])
+    assert!(status.success());
+    status = Command::new("ar").args(&["crus", "libcimd.a", "cimd.o"])
                       .current_dir(&Path::new(&out_dir))
                       .status().unwrap();
+    assert!(status.success());
+
     println!("cargo:rustc-link-search=native={}", out_dir);
     println!("cargo:rustc-link-lib=static=cimd");
 }
