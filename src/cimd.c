9d52640219abfe19e2501ec93ac79a35e5bf1bc9
Rémi Garde <remi.garde@free.fr>
2019-01-23 18:56:31 +0100

    C SIMD: use supported functions that use unalgned pointers

--- src/cimd.c
+++ src/cimd.c
@@ -8,11 +8,10 @@
 void add_simd_c512(int * data, int * datb, int * dst, int n) {
     for (int i = 0 ; i < n ; i  = i+16) {
         // Get the first 8 i32 in a SIMD type
-        __m512i veca = _mm512_load_epi32((__m512i *)(data+i));
-        __m512i vecb = _mm512_load_epi32((__m512i *)(datb+i));
-
+        __m512i veca = _mm512_loadu_si512((__m512i *)(data+i));
+        __m512i vecb = _mm512_loadu_si512((__m512i *)(datb+i));
         // Store the addition result in dst
-        _mm512_store_epi32(dst+i, _mm512_add_epi32(veca, vecb));
+        _mm512_storeu_si512((__m512i *)(dst+i), _mm512_add_epi32(veca, vecb));
     }
 }
 #endif
@@ -26,7 +25,7 @@ void add_simd_c256(int * data, int * datb, int * dst, int n) {
         __m256i vecb = _mm256_loadu_si256((__m256i *) (datb+i));
 
         // Store the addition result in dst
-        _mm256_store_si256(dst+i, _mm256_add_epi32(veca, vecb));
+        _mm256_storeu_si256((__m256i *)(dst+i), _mm256_add_epi32(veca, vecb));
     }
 }
 #endif // No SIMD
