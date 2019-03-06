6b3f2d3ef72aa2c7a7ddd2b3479135078c799ea9
Rémi Garde <remi.garde@free.fr>
2019-03-06 21:43:23 +0100

    Fix(runner): parse correctly when the bench name is too long and on a separate line

--- run_criterion.py
+++ run_criterion.py
@@ -5,15 +5,15 @@ import os
 import json
 import sys
 
-def parse_line(line):
-    #  line = 'addSIMD256/262144       time:   [32.801 us 33.488 us 34.241 us]'
+def parse_line(line, name):
+    #  line = '     time:   [32.801 us 33.488 us 34.241 us]'
+    line = line.split('time:')[1]
     words = line.split()
-    # Parse the benchmark ID: name/size
-    name = words[0].split('/')
+    # Parse the benchmark ID: name/size given as arg as if it is too long it is not provided on the same line
     benchmark = name[0]
-    array_length = name[1]
+    array_length = name[1][:-1]
     # parse the time unit to convert to ns
-    unit = words[3]
+    unit = words[1]
     multiplier = 1
     if unit == 's':
         multiplier = 1000000000
@@ -22,9 +22,9 @@ def parse_line(line):
     elif unit == 'us':
         multiplier = 1000
     # Parse the results
-    score = float(words[4])*multiplier
-    confidencemin = float(words[2][1:])*multiplier
-    confidencemax = float(words[6])*multiplier
+    score = float(words[2])*multiplier
+    confidencemin = float(words[0][1:])*multiplier
+    confidencemax = float(words[4])*multiplier
     scoreError = (confidencemax - confidencemin) / 2
     scoreConfidence = [confidencemin, confidencemax]
     return {
@@ -40,24 +40,33 @@ def parse_line(line):
         }
     }
 
-def run(minsize, maxsize, extraArgs):
+def run(minsize, maxsize, filename, extraArgs):
     p = subprocess.Popen(['cargo', 'bench', '--', '--color=never'] + extraArgs, stdout=subprocess.PIPE, env=dict(os.environ,ARRAY_LENGTH_MIN=str(minsize) , ARRAY_LENGTH_MAX=str(maxsize)))
     results = []
+    name = ''
+    file = open(filename, 'w')
+    file.write("[\n")
+    first=True
     while True:
         line = p.stdout.readline().decode()
         if line == '' and p.poll() is not None:
             break
         if line:
             if 'Warming up' in line or 'Collecting' in line:
+                name = line.split()[1].split('/')
                 print(line, file=sys.stderr)
             elif 'time:' in line:
-                results.append(parse_line(line))
-
-    print(json.dumps(results, indent=4, sort_keys=True))
+                if not first:
+                    file.write(',\n')
+                first = False
+                file.write(json.dumps(parse_line(line, name), indent=4, sort_keys=True))
+                file.flush()
+    file.write('\n]')
+    file.close()
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        print("Usage: run_criterion.py <minsize> <maxsize> [extra cargo args]\nIf a size is less than 64, it is interpreted a a power of 2")
+        print("Usage: run_criterion.py <minsize> <maxsize> <outfile> [extra cargo args]\nIf a size is less than 64, it is interpreted a a power of 2")
     else:
         minsize = int(sys.argv[1])
         if minsize < 64:
@@ -65,4 +74,4 @@ if __name__ == "__main__":
         maxsize = int(sys.argv[2])
         if maxsize < 64:
             maxsize  = 1 << maxsize
-        run(minsize, maxsize, sys.argv[3:])
+        run(minsize, maxsize, sys.argv[3], sys.argv[4:])
